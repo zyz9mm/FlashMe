4836a7858fa15387bc31e5ae7c50d9562334700f
Justin Lee <zyz9mm@virginia.edu>
2025-06-12 14:11:16 -0400

    Frontend start

---
+++ FlashMeFrontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/header'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+ return (
+    <div>
+      <Header />
+      <p>This is the homepage.</p>
+    </div>
+  )
+}
+
+export default App
---
+++ FlashMeFrontend/src/components/header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <h1>FlashMe</h1>
+}
+
+export default Header
